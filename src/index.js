f5a2c5dfdca209b199aaf2bb2fbe6b922629f927
Chengyonggang521 <1013531201@qq.com>
2024-06-24 12:43:07 +0800

    Update index.js
    const routes = {
      // production
      "docker.yeexun.com.cn": dockerHub,
      "quay.yeexun.com.cn": "https://quay.io",
      "gcr.yeexun.com.cn": "https://gcr.io",
      "k8s-gcr.yeexun.com.cn": "https://k8s.gcr.io",
      "k8s.yeexun.com.cn": "https://registry.k8s.io",
      "ghcr.yeexun.com.cn": "https://ghcr.io",
      "cloudsmith.yeexun.com.cn": "https://docker.cloudsmith.io",

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.yeexun.com.cn": dockerHub,
+  "quay.yeexun.com.cn": "https://quay.io",
+  "gcr.yeexun.com.cn": "https://gcr.io",
+  "k8s-gcr.yeexun.com.cn": "https://k8s.gcr.io",
+  "k8s.yeexun.com.cn": "https://registry.k8s.io",
+  "ghcr.yeexun.com.cn": "https://ghcr.io",
+  "cloudsmith.yeexun.com.cn": "https://docker.cloudsmith.io",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
